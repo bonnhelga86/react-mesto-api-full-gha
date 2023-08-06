33fac439a737e22e8124c5463d243705938e728d
Olga Bondarenko <bonnhelga86@yandex.ru>
2023-08-06 08:40:55 +0400

    refactor: Поменять порт для frontend

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-  origin: 'https://bonnhelga86.nomoreparties.co/',
+  origin: 'https://bonnhelga86.nomoreparties.co',
   credentials: true,
 }));
 
